5fa2649dd0d6c8bc6e3e754ea2b919802c2f68bd
Szymon Mielecki <szymon.mielecki@gmail.com>
2025-07-19 10:38:17 +0200

    initial commit

---
+++ src/main/java/org/szymonmielecki/recruitmenttaskatipera/RecruitmentTaskAtiperaApplication.java
@@ -0,0 +1,13 @@
+package org.szymonmielecki.recruitmenttaskatipera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentTaskAtiperaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentTaskAtiperaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/szymonmielecki/recruitmenttaskatipera/RecruitmentTaskAtiperaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.szymonmielecki.recruitmenttaskatipera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskAtiperaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
